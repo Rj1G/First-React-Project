20ce82f45e893f35fdb5d4ebe4935b16cfa76653
Rj1G <rj.chaudhary02@gmail.com>
2024-09-01 12:58:48 +0530

    Hello2

---
+++ src/Hello2.tsx
@@ -0,0 +1,25 @@
+export default function HelloWorld(){
+    const a = 10;
+    const link = "https://cdn.britannica.com/89/149189-050-68D7613E/Bengal-tiger.jpg";
+    const addition = (value:number)=>value+10;
+
+    function Add(){
+        let a=11;
+        alert (a);
+    }
+    return (
+        <>
+        <h1>   Hello World........!!</h1>
+        <h2>    Welcome in my Country India...............!!!</h2>
+
+        <span>   The value a double is {a*2}</span>
+        <img src={link} />
+        <span>    Add 10 number to given number {addition(5)}...............!!!</span>
+
+        <button onClick={()=>{
+            alert("hi.....I am SalMan Khan");
+        }}>Submit</button>
+        <button onClick={Add}>ADD</button>
+        </>
+    )
+}
